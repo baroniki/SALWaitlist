d09a6682b17a82d86ba8df61570f3183c5b88feb
Brighton Balfrey <balfrey@usc.edu>
2018-11-08 17:17:53 -0800

    Students

---
+++ SALWaitlist/Students.java
@@ -0,0 +1,10 @@
+import java.util.ArrayList;
+
+public class Students {
+	public String Email = null;
+	public String Fname = null;
+	public String Lname = null;
+	public String Profile_Picture_URL = null;
+	public String Phone_Number = null;
+	public ArrayList<Integer> classes = null;
+}
