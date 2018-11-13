68036759e6db94bb20c296139a6d6bfbbe9ea4af
danielhkim96 <kim321@usc.edu>
2018-11-12 19:29:38 -0800

    Update script.js

--- SALWaitlist/WebContent/script.js
+++ SALWaitlist/WebContent/script.js
@@ -6,47 +6,84 @@ function onSignIn(googleUser) {
 		type: "POST",
 		url: "LogInServlet",
 		data: {
+			'action': "authenticate",
 			'email': GoogleUser.getBasicProfile().getEmail(),
 			'name': GoogleUser.getBasicProfile().getName(),
-			'profpic': GoogleUser.getBasicProfile().getImageUrl()
-		}, success: function(response) {
+			'imgURL': GoogleUser.getBasicProfile().getImageUrl()
+		},
+		success: function(response) {
 			if (response === "cp") {
 				$.ajax({
 					type: "POST",
-					url: "cp-home.jsp",
+					url: "LogInServlet",
 					data: {
+						'action': "redirect",
+						'nextPage': "/cp-home.jsp",
 						'email': GoogleUser.getBasicProfile().getEmail(),
 						'name': GoogleUser.getBasicProfile().getName(),
-						'profpic': GoogleUser.getBasicProfile().getImageUrl()
-					}, success: function() {
+						'imgURL': GoogleUser.getBasicProfile().getImageUrl()
+					},
+					success: function() {
 						window.location.replace("cp-home.jsp");
 					}
 				});
 			} else if (response === "student") {
 				$.ajax({
 					type: "POST",
-					url: "cp-home.jsp",
+					url: "LogInServlet",
 					data: {
+						'action': "redirect",
+						'nextPage': "/student-home.jsp",
 						'email': GoogleUser.getBasicProfile().getEmail(),
 						'name': GoogleUser.getBasicProfile().getName(),
-						'profpic': GoogleUser.getBasicProfile().getImageUrl()
-					}, success: function() {
+						'imgURL': GoogleUser.getBasicProfile().getImageUrl()
+					},
+					success: function() {
 						window.location.replace("student-home.jsp");
 					}
 				});
 			} else if (response === "new") {
 				$.ajax({
 					type: "POST",
-					url: "cp-home.jsp",
+					url: "LogInServlet",
 					data: {
+						'action': "redirect",
+						'nextPage': "/profile.jsp",
 						'email': GoogleUser.getBasicProfile().getEmail(),
 						'name': GoogleUser.getBasicProfile().getName(),
-						'profpic': GoogleUser.getBasicProfile().getImageUrl()
-					}, success: function() {
+						'imgURL': GoogleUser.getBasicProfile().getImageUrl()
+					},
+					success: function() {
 						window.location.replace("profile.jsp");
 					}
 				});
 			}
 		}
 	});
+}
+
+function updateProfile(email) {	
+	var phoneNumber = document.getElementById("phoneNumber").value;
+	var format = /^\d{10}$/;
+	if (phoneNumber.match(format)) {
+		var checked = [];
+		$("input:checkbox:checked").each(function(){
+		    checked.push($(this).val());
+		});
+		
+		$.ajax({
+			type: "POST",
+			url: "ProfileServlet",
+			data: {
+				'email': email,
+				'phoneNumber': phoneNumber,
+				'classes': JSON.stringify(checked)
+			},
+			success: function() {
+				alert("Profile successfully updated!");
+			}
+		});
+	} else {
+		alert("Invalid number. Please enter 10 digits.");
+	}
 }
