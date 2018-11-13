b9f6bd4f22663b4a16c07a471bcea1d19d471bb8
aahadpatelusc <aahadpat@usc.edu>
2018-11-12 19:17:31 -0800

    update addtowaitlist

--- SALWaitlist/src/AddToWaitlist.java
+++ SALWaitlist/src/AddToWaitlist.java
@@ -1,10 +1,8 @@
 
 
 import java.io.IOException;
-//import java.io.PrintWriter;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
-//import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 
