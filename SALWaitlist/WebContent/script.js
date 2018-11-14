f31d45dc9c80ab73b97fe83e9cd99922517a2ab9
danielhkim96 <kim321@usc.edu>
2018-11-13 21:01:45 -0800

    Update script.js

--- SALWaitlist/WebContent/script.js
+++ SALWaitlist/WebContent/script.js
@@ -13,35 +13,10 @@ function onSignIn(googleUser) {
 		},
 		success: function(response) {
 			if (response === "cp") {
-				$.ajax({
-					type: "POST",
-					url: "LogInServlet",
-					data: {
-						'action': "redirect",
-						'nextPage': "/cp-home.jsp",
-						'email': GoogleUser.getBasicProfile().getEmail(),
-						'name': GoogleUser.getBasicProfile().getName(),
-						'imgURL': GoogleUser.getBasicProfile().getImageUrl()
-					},
-					success: function() {
-						window.location.replace("cp-home.jsp");
-					}
-				});
+				window.location.replace("cp-home.jsp");
 			} else if (response === "student") {
-				$.ajax({
-					type: "POST",
-					url: "LogInServlet",
-					data: {
-						'action': "redirect",
-						'nextPage': "/student-home.jsp",
-						'email': GoogleUser.getBasicProfile().getEmail(),
-						'name': GoogleUser.getBasicProfile().getName(),
-						'imgURL': GoogleUser.getBasicProfile().getImageUrl()
-					},
-					success: function() {
-						window.location.replace("student-home.jsp");
-					}
-				});
+
+				window.location.replace("student-home.jsp");
 			} else if (response === "new") {
 				$.ajax({
 					type: "POST",
@@ -62,7 +37,32 @@ function onSignIn(googleUser) {
 	});
 }
 
-function updateProfile(email) {	
+function loadProfile() {
+	setTimeout(function() {
+		var img = document.createElement("img"); 
+		img.src = gapi.auth2.getAuthInstance().currentUser.get().getBasicProfile().getImageUrl();
+		var src = document.getElementById("profilePic"); 
+		src.appendChild(img);
+		document.getElementById("profileName").innerHTML = gapi.auth2.getAuthInstance().currentUser.get().getBasicProfile().getName();
+	}, 100);
+}
+
+function loadTable() {
+	setTimeout(function() {
+		$.ajax({
+			type: "POST",
+			url: "WaitlistServlet",
+			data: {
+				'email': gapi.auth2.getAuthInstance().currentUser.get().getBasicProfile().getEmail()
+			},
+			success: function(result) {
+				console.log(result);
+			}
+		});
+	}, 100);
+}
+
+function updateProfile() {	
 	var phoneNumber = document.getElementById("phoneNumber").value;
 	
 	if (phoneNumber === "") {
@@ -75,7 +75,7 @@ function updateProfile(email) {
 			type: "POST",
 			url: "ProfileServlet",
 			data: {
-				'email': email,
+				'email': gapi.auth2.getAuthInstance().currentUser.get().getBasicProfile().getEmail(),
 				'phoneNumber': phoneNumber,
 				'classes': JSON.stringify(checked)
 			},
@@ -95,7 +95,7 @@ function updateProfile(email) {
 				type: "POST",
 				url: "ProfileServlet",
 				data: {
-					'email': email,
+					'email': gapi.auth2.getAuthInstance().currentUser.get().getBasicProfile().getEmail(),
 					'phoneNumber': phoneNumber,
 					'classes': JSON.stringify(checked)
 				},
@@ -106,5 +106,38 @@ function updateProfile(email) {
 		} else {
 			alert("Invalid phone number. Please enter 10 digits.");
 		}
+	}
+}
+
+function helpStudent(name, index) {
+	var current = document.getElementById("current");
+	
+	if (document.getElementById("startCheck" + index).checked === true) {
+		current.innerHTML = "You are currently helping " + name + ".";
+		current.style.visibility = "visible";
+		document.getElementById("finishCheck" + index).disabled = false;
+	} else {
+		current.style.visibility = "hidden";
+		document.getElementById("finishCheck" + index).disabled = true;
+	}
+}
+
+function removeStudent(email, name, index) {
+	var confirmation = confirm("Are you done helping " + name + "?");
+	if (confirmation) {
+		$.ajax({
+			type: "POST",
+			url: "RemoveFromWaitlist",
+			data: {
+				'email': email
+			},
+			success: function() {
+				document.getElementById("current").style.visibility = "hidden";
+				document.getElementById("startCheck" + index).disabled = true;
+				document.getElementById("finishCheck" + index).disabled = true;
+			}
+		});
+	} else {
+		document.getElementById("finishCheck" + index).checked = false;
 	}
 }
